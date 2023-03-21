2fab241bedf16715a8453f61b78f327b51983711
Brunno <brunnopereira090@gmail.com>
2023-03-21 10:49:57 -0300

    JS modificado

---
+++ index.js
@@ -0,0 +1,30 @@
+let valor_cidade = []
+let res = document.getElementById('res')
+
+function inserir(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.push(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+function insert_inicio(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+
+
+function excluir(){
+    valor_cidade.pop()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+function excluir_final(){
+    valor_cidade.shift()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
